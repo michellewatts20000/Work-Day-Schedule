585baee136a495c0ea5566d9093290825d14fd6d
michellewatts20000 <watts.e.michelle@gmail.com>
2021-03-27 16:28:03 +1100

    fixed up time comparisonissue on the time-block class

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,61 +15,61 @@ function time() {
 }
 
 
-// // checks every minute if colours need to change
-// function colourUpdate() {
-//     // makes a current hour variable using moment
-//     var simpleTime = moment().hours();
-
-//     // looks at every .time-block and stores the value of its ID and then compares it to the current time and gives it a class to show if the time has passed or not
-//     $(".time-block").each(function () {
-//         var blockTime = parseInt($(this).attr("id"));
-//         if (blockTime < simpleTime) {
-//             $(this).removeClass("future");
-//             $(this).removeClass("present");
-//             $(this).addClass("past");
-//         }   else if (blockTime === simpleTime) {
-//             $(this).removeClass("past");
-//             $(this).removeClass("future");
-//             $(this).addClass("present");
-//         }
-//         else {
-//             $(this).removeClass("present");
-//             $(this).removeClass("past");
-//             $(this).addClass("future");
-
-//         }
-
-//     })
-
-
-// }
-
-
-
+// checks every minute if colours need to change
 function colourUpdate() {
     // makes a current hour variable using moment
     var simpleTime = moment().hours();
 
-    $(".hour").each(function () {
-        var blockTime = $(this).text().replaceAll("AM", "").replaceAll("PM", "");
+    // looks at every .time-block and stores the value of its ID and then compares it to the current time and gives it a class to show if the time has passed or not
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id"));
         if (blockTime < simpleTime) {
-            $(".time-block").removeClass("future");
-            $(".time-block").removeClass("present");
-            $(".time-block").addClass("past");
-        } else if (blockTime === simpleTime) {
-            $(".time-block").removeClass("past");
-            $(".time-block").removeClass("future");
-            $(".time-block").addClass("present");
-        } else {
-            $(".time-block").removeClass("present");
-            $(".time-block").removeClass("past");
-            $(".time-block").addClass("future");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }   else if (blockTime === simpleTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
 
         }
 
     })
 
+
 }
+
+
+
+// function colourUpdate() {
+//     // makes a current hour variable using moment
+//     var simpleTime = moment().hours();
+// console.log(simpleTime);
+//     $(".hour").each(function () {
+//         var blockTime = $(this).text().replaceAll("AM", "").replaceAll("PM", "");
+//         if (blockTime < simpleTime) {
+//             $(".time-block").removeClass("future");
+//             $(".time-block").removeClass("present");
+//             $(".time-block").addClass("past");
+//         } else if (blockTime === simpleTime) {
+//             $(".time-block").removeClass("past");
+//             $(".time-block").removeClass("future");
+//             $(".time-block").addClass("present");
+//         } else {
+//             $(".time-block").removeClass("present");
+//             $(".time-block").removeClass("past");
+//             $(".time-block").addClass("future");
+
+//         }
+
+//     })
+
+// }
 
 
 
@@ -91,6 +91,12 @@ function setLS (){
     $("#9 .description").val(localStorage.getItem("9"));
     $("#10 .description").val(localStorage.getItem("10"));
     $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
 }
 
