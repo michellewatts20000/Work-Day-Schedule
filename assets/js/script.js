11ad8403972e94b1deb0736c67efa85789e68539
michellewatts20000 <watts.e.michelle@gmail.com>
2021-03-27 13:11:07 +1100

    fixing up conditional loop and date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,47 @@
 // formats date and displays it in the jumbotron
-var currentTime = moment().format("dddd, MMMM Do, h:mm:ss a");
-var simpleTime = moment().format("HH:mm:ss");
+// var currentTime = moment().format("dddd, MMMM Do, h:mm:ss a");
+
+//  calls the currenttime function
+time();
+
+// 
+setInterval(time, 1000);
 
 function time() {
+    var currentTime = moment().format("dddd, MMMM Do, h:mm:ss a");
     $("#currentDay").text(currentTime);
 }
 
-time();
-setInterval(time, 1000);
+
+
 
 
 // on click save button store textfield to localstorage
 
 var str = $("textfield").text();
 
-$("#clickEvent").click(function() {
-    alert( "Handler for .click() called." );
-  });
+console.log(str);
+
+$("#clickEvent").click(function () {
+    alert("Handler for .click() called.");
+});
 
 
 
 // store the time and compare to actual time
-var hour9 = "09:00:00";
+var hour9 = 9;
 
+// makes an hour variable
+var simpleTime = moment().hours();
 
 console.log(hour9);
 console.log(simpleTime);
 
 
-if (hour9 < simpleTime)
-{
-    $("#hour9").addClass("past");
-
-} else if (hour9 > simpleTime); {
-    $("#hour9").addClass("current");
-} 
-
-// } else {
-//     $("#hour9").addClass("future");
-// }
+if (hour9 < simpleTime) {
+    $("#hour9").addClass("past")
+} else if (hour9 > simpleTime) {
+    $("#hour9").addClass("future")
+} else {
+    $("#hour9").addClass("present")
+};
