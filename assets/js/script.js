43f141b1fe91bc761a0783a998139fe152a5baee
michellewatts20000 <watts.e.michelle@gmail.com>
2021-03-28 11:17:15 +1100

    used the data attribute to store the textarea value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-//  calls the currenttime function every second
+//  calls the currentTime function every second
 time();
 setInterval(time, 1000);
 
-//  calls the colour update function every 60sec
+//  calls the colourUpdate function every 60sec
 colourUpdate();
 setInterval(colourUpdate, 60000);
 
@@ -15,55 +15,50 @@ function time() {
     $("#currentDay").text(currentTime);
 }
 
-
 // checks every minute if colours need to change
 function colourUpdate() {
     // makes a current hour variable using moment
     var simpleTime = moment().hours();
 
-    // looks at every .time-block and stores the value of its ID and then compares it to the current time and gives it a class to show if the time has passed or not
+    // looks at every .time-block and stores the value of its specific data attribute and then compares it to the current time and gives it a class to show if the time has passed or not
     $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id"));
+        var blockTime = parseInt($(this).attr("data"));
         if (blockTime < simpleTime) {
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
-        }   else if (blockTime === simpleTime) {
+        } else if (blockTime === simpleTime) {
             $(this).removeClass("past");
             $(this).removeClass("future");
             $(this).addClass("present");
-        }
-        else {
+        } else {
             $(this).removeClass("present");
             $(this).removeClass("past");
             $(this).addClass("future");
-
         }
     })
 }
 
-// when saveBtn is clicked store the value inside all textareas and also store their parent ID
-$(".saveBtn").click(function () {
-    var time = $(this).parent().attr("id");
+// when saveBtn is clicked store the textarea value and also store its specific data attribute
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var time = $(this).parent().attr("data");
     var text = $(this).siblings(".description").val();
-    
     // Save text in local storage
     localStorage.setItem(time, text);
 });
 
-
 // get local storage when page is reloaded
-function setLS (){
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#13 .description").val(localStorage.getItem("13"));
-    $("#14 .description").val(localStorage.getItem("14"));
-    $("#15 .description").val(localStorage.getItem("15"));
-    $("#16 .description").val(localStorage.getItem("16"));
-    $("#17 .description").val(localStorage.getItem("17"));
-
+function setLS() {
+    $("[data='9'] .description").val(localStorage.getItem("9"));
+    $("[data='10'] .description").val(localStorage.getItem("10"));
+    $("[data='11'] .description").val(localStorage.getItem("11"));
+    $("[data='12'] .description").val(localStorage.getItem("12"));
+    $("[data='13'] .description").val(localStorage.getItem("13"));
+    $("[data='14'] .description").val(localStorage.getItem("14"));
+    $("[data='15'] .description").val(localStorage.getItem("15"));
+    $("[data='16'] .description").val(localStorage.getItem("16"));
+    $("[data='17'] .description").val(localStorage.getItem("17"));
 }
 
 // clear local storage
